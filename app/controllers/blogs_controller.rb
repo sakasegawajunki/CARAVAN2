70f1ffefa4088989707e248b901d4d25233095f0
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-05-06 13:35:33 +0000

    [Finish]4-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def index
+   @blogs =Blog.all
   end
 
   def show
@@ -10,8 +11,17 @@ class BlogsController < ApplicationController
   end
   
   def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
   end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+  
 end
